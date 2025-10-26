6ca04f1f445a6c158def6aa6327905fde8df214b
Edward Tait <139652245+edwardtait84@users.noreply.github.com>
2025-10-26 15:59:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.tab-button');
+const contents = document.querySelectorAll('.tab-content');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active'));
+    contents.forEach(c => c.classList.remove('active'));
+
+    button.classList.add('active');
+    document.getElementById(button.dataset.tab).classList.add('active');
+  });
+});
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('main-nav');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
